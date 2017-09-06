adce10bbd49d6be15d367535fc1a81bb5ee171a3
Donald Dunbar <donald.l.dunbar@gmail.com>
2017-09-06 01:18:33 -0700

    Init & CameraRotateAround script

---
+++ Assets/CameraRotateAround.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotateAround : MonoBehaviour {
+
+
+
+
+	private Transform trans;
+
+	public float speed = 10f;
+	public Transform rotationTarget;
+
+
+	void Start () {
+		transform.LookAt (rotationTarget.position);
+	}
+	
+	void FixedUpdate () {
+		transform.RotateAround (rotationTarget.position, Vector3.up, speed * Time.deltaTime);
+
+	
+	}
+}

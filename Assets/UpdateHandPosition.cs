db01b588fc883db745b8e0a574f6f21918759b14
Donald Dunbar <donald.l.dunbar@gmail.com>
2017-09-07 01:01:46 -0700

    Made TinyDancer, starting on environment and particle systems

---
+++ Assets/UpdateHandPosition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateHandPosition : MonoBehaviour {
+
+	public GameObject cont;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+
+	void FixedUpdate () {
+		rb.MoveRotation (cont.transform.rotation);
+		rb.MovePosition (cont.transform.position);
+	}
+}
